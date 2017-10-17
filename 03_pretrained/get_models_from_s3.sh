f44c58595ba5079178a23ff0c8f1753e7ca22111
Julien Simon <julien@julien.org>
2017-10-17 16:36:14 +0200

    Initial version

---
+++ 03_pretrained/get_models_from_s3.sh
@@ -0,0 +1,7 @@
+aws s3 cp s3://jsimon-mxnetworkshop/vgg16-symbol.json .
+aws s3 cp s3://jsimon-mxnetworkshop/vgg16-0000.params .
+aws s3 cp s3://jsimon-mxnetworkshop/Inception-BN-symbol.json .
+aws s3 cp s3://jsimon-mxnetworkshop/Inception-BN-0000.params .
+aws s3 cp s3://jsimon-mxnetworkshop/resnet-152-symbol.json .
+aws s3 cp s3://jsimon-mxnetworkshop/resnet-152-0000.params .
+aws s3 cp s3://jsimon-mxnetworkshop/synset.txt .

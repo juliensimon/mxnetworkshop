Fix time units

06_piPolly/solution/classify.py
@@ -35,7 +35,7 @@ def predict(filename, model, categories, n):
 	model.forward(Batch([array]))
     	prob = model.get_outputs()[0].asnumpy()
 	t2 = time.time()
-	print "Predicted in %.2f microseconds" % (t2-t1)
+	print (t2-t1)
 
 	prob = np.squeeze(prob)
     	sortedprobindex = np.argsort(prob)[::-1]
@@ -62,7 +62,7 @@ def build_top1_message(topN):
 if __name__ == "__main__":
 
 	filename = "myimage.jpg"
-	takePicture(filename)
+	#takePicture(filename)
 
 	inceptionv3,c = init("Inception-BN")
 	top5 = predict(filename,inceptionv3,c,5)
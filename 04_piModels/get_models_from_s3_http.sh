2cd0291bcac6704108a61edcfed74d43845be11e
Julien Simon <julien@julien.org>
2017-11-22 13:17:41 +0100

    Initial version

---
+++ 04_piModels/get_models_from_s3_http.sh
@@ -0,0 +1,7 @@
+wget https://s3-eu-west-1.amazonaws.com/jsimon-mxnetworkshop/vgg16-symbol.json .
+wget https://s3-eu-west-1.amazonaws.com/jsimon-mxnetworkshop/vgg16-0000.params .
+wget https://s3-eu-west-1.amazonaws.com/jsimon-mxnetworkshop/Inception-BN-symbol.json .
+wget https://s3-eu-west-1.amazonaws.com/jsimon-mxnetworkshop/Inception-BN-0000.params .
+wget https://s3-eu-west-1.amazonaws.com/jsimon-mxnetworkshop/resnet-152-symbol.json .
+wget https://s3-eu-west-1.amazonaws.com/jsimon-mxnetworkshop/resnet-152-0000.params .
+wget https://s3-eu-west-1.amazonaws.com/jsimon-mxnetworkshop/synset.txt .

2370b671b71b50ed6aa673a21639bdb9b64974af
Julien Simon <julien@julien.org>
2017-11-24 15:19:06 +0100

    Add opencv

--- 03_piSetup/setup.sh
+++ 03_piSetup/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y wget python python-dev gcc awscli
+sudo apt-get install -y wget python python-dev gcc awscli python-opencv
 sudo apt-get -y remove python-pip
 wget https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py
 sudo pip install mxnet # Don't use --upgrade: this could force numpy to be rebuilt from source

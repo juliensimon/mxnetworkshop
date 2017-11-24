f4e6a2ef5c5be57531bb3826d19cb8b0b7b174bf
Julien Simon <julien@julien.org>
2017-11-24 11:47:22 +0000

    Remove version requirement

--- 03_piSetup/setup.sh
+++ 03_piSetup/setup.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install -y wget python python-dev gcc awscli
 sudo apt-get -y remove python-pip
 wget https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py
-sudo pip install mxnet==0.12.1 # Don't use --upgrade: this could force numpy to be rebuilt from source
+sudo pip install mxnet # Don't use --upgrade: this could force numpy to be rebuilt from source
 /bin/rm -f get-pip.py

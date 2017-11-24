e0bd0768d42adcf0cba1b517bb8195d7a877ffc7
Julien Simon <julien@julien.org>
2017-11-24 11:18:00 +0000

    Fixes

--- 03_piSetup/setup.sh
+++ 03_piSetup/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y wget python python-dev gcc awscli
-sudo apt-get uninstall -y pip
+sudo apt-get -y remove python-pip
 wget https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py
 sudo pip install mxnet==0.12.1 # Don't use --upgrade: this could force numpy to be rebuilt from source
+/bin/rm -f get-pip.py
